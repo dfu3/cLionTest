b0fff8e3e01d8386afb0f7f92f78babd59f47a9c
Osru <osrudante@gmail.com>
2017-01-19 10:48:05 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/testGit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testGit.dir/main.cpp.obj"
+  "testGit.pdb"
+  "testGit.exe"
+  "testGit.exe.manifest"
+  "libtestGit.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testGit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
